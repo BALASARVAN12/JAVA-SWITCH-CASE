2374f3ffa221d2ae6841dfedaf2ee03da8f4d321
BALASARVAN12 <91976828+BALASARVAN12@users.noreply.github.com>
2021-10-22 21:16:30 +0530

    Update MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -3,7 +3,7 @@ public class MatrixAddition  {
 	
 	      int c[][]=new int[3][3];
 	      int a[][]={{1,3,4},{2,4,3},{3,4,5}};
-        int b[][]={{1,3,4},{2,4,3},{1,2,4}};
+              int b[][]={{1,3,4},{2,4,3},{1,2,4}};
         
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
